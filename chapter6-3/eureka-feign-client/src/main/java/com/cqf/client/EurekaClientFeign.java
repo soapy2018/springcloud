96906d0d42ec7f81957af05fdc94d35e65732231
soapy2018 <soapy2018@hotmail.com>
2019-12-19 15:28:59 +0800

    5

---
+++ chapter6-3/eureka-feign-client/src/main/java/com/cqf/client/EurekaClientFeign.java
@@ -0,0 +1,15 @@
+package com.cqf.client;
+
+import com.cqf.client.config.FeignConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by cqf on 2019/8/14
+ */
+@FeignClient(value = "eureka-client",configuration = FeignConfig.class)
+public interface EurekaClientFeign {
+    @GetMapping(value = "/hi")
+    String sayHiFromClientEureka(@RequestParam(value = "name") String name);
+}
---
+++ chapter6-3/eureka-feign-client/src/main/java/com/cqf/web/HiController.java
@@ -0,0 +1,20 @@
+package com.cqf.web;
+
+import com.cqf.service.HiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fangzhipeng on 2017/6/21.
+ */
+@RestController
+public class HiController {
+    @Autowired
+    HiService hiService;
+    @GetMapping("/hi")
+    public String sayHi(@RequestParam( defaultValue = "cqf",required = false)String name){
+        return hiService.sayHi(name);
+    }
+}
