4b422979fc3e32955a2b29bcec02fec4c1fad45e
soapy2018 <soapy2018@hotmail.com>
2019-12-30 14:18:37 +0800

    init

---
+++ chapter12/eureka-client-two/src/main/java/com/cqf/EurekaClientTwoApplication.java
@@ -0,0 +1,26 @@
+package com.cqf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientTwoApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+	@GetMapping("/hi")
+	public String home(@RequestParam String name) {
+		return "hi "+name+",i  am lucy and from port:" +port;
+	}
+}
