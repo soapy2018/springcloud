5dfe9f123a14985f531a26e0d334f7c78aa448f7
soapy2018 <soapy2018@hotmail.com>
2021-01-27 14:11:54 +0800

    修改目录名

--- 2-整合JPA/src/main/java/com/cqf/Chapter2JPAApplication.java
+++ 2-整合JPA/src/main/java/com/cqf/Chapter2JPAApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter45Application {
+public class Chapter2JPAApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter45Application.class, args);
+		SpringApplication.run(Chapter2JPAApplication.class, args);
 	}
 }
---
+++ 2-整合JPA/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+insert into user(username, password) values ('CQF', '123456');
